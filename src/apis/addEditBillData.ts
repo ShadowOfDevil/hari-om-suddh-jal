4f6cfe376148d884e9b6d5dbabc3f8b77e42069c
ajay-prajapati <ajayprajapati20031999@gmail.com>
2024-08-10 13:51:19 +0530

    initial commit

--- src/apis/addEditBillData.ts
+++ src/apis/addEditBillData.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import { Bill } from "../dataType";
 
 export const addEditBillData = async (formData: Bill, id?: string) => {
-  let route = `${import.meta.env.VITE_REACT_APP_API_URL}bills`;
+  let route = `${process.env.VITE_REACT_APP_API_URL}bills`;
 
   if (id) {
-    route = `${import.meta.env.VITE_REACT_APP_API_URL}bills/${id}`;
+    route = `${process.env.VITE_REACT_APP_API_URL}bills/${id}`;
   }
 
   const response = id ? await axios.put(route, formData) : await axios.post(route, formData);
--- src/apis/deleteBillsData.ts
+++ src/apis/deleteBillsData.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const deleteBillData = async (id: string) => {
-  let route = `${import.meta.env.VITE_REACT_APP_API_URL}bills/${id}`;
+  let route = `${process.env.VITE_REACT_APP_API_URL}bills/${id}`;
 
   const response = await axios.delete(route);
 
